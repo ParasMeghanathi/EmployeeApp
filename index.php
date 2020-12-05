86801e00517d0eee5041b3cec824d9f91df6d0a3
ParasMeghanathi <57862275+ParasMeghanathi@users.noreply.github.com>
2020-12-05 23:52:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+		
+    </head>
+    <?php
+    if (isset($_POST['add'])) {
+        $host = 'localhost';
+        $user = 'root';
+        $password = '';
+        $database = 'emp_details';
+        $port = 3306;
+        /*$host = 'database-1.c6jevgf51ssc.us-east-1.rds.amazonaws.com';
+        $user = 'admin';
+        $password = '123admin';
+        $database = 'StudentDB';
+        $port = 3306;*/
+
+        $emp_name = $_POST['emp_name'];
+        $emp_email = $_POST['emp_email'];
+        $emp_salary = $_POST['emp_salary'];
+        $emp_phone = $_POST['emp_phone'];
+        $emp_job = $_POST['emp_job'];
+		
+        $conn = mysqli_connect($host, $user, $password, $database, $port);
+
+		
+
+        if ($conn) {
+            
+        } else {
+            die("Database Connection Failed.");
+        }
+		
+        $query = "INSERT INTO `emp`(`name`, `email`, `salary`, `phone`, `job`) VALUES ('" . $emp_name . "','" . $emp_email . "','" . $emp_salary . "','" . $emp_phone . "','" . $emp_job . "')";
+
+        $res = mysqli_query($conn, $query);
+		
+		
+        if ($res > 0) {
+            header('location:view.php');
+        }
+    }
+    ?>
+    <body bgcolor='#ADD8E6'>
+    <center>
+        <h1 style="color:#FF6347;">EmployeeDetails</h1>
+		
+        <form method = "post">
+            <table width = "400" border = "0" cellspacing = "1" 
+                   cellpadding = "2">
+				  
+
+                <tr>
+                    <td width = "100">EmployeeName:</td>
+                    <td><input name = "emp_name" type = "text" 
+                               id = "emp_name"></td>
+                </tr>
+
+                <tr>
+                    <td width = "100">EmployeeEmail:</td>
+                    <td><input name = "emp_email" type = "text" 
+                               id = "emp_email"></td>
+                </tr>
+
+                <tr>
+                    <td width = "100">EmployeeSalary</td>
+                    <td><input name = "emp_salary" type = "text" 
+                               id = "emp_salary"></td>
+                </tr>
+                <tr>
+                    <td width = "100">EmployeePhone</td>
+                    <td><input name = "emp_phone" type = "text" 
+                               id = "emp_phone"></td>
+                </tr>
+                <tr>
+                    <td width = "100">Job-Name</td>
+                    <TD width="50%"><select name="emp_job" id = "emp_job"> 
+                            <OPTION value="Manager">Manager</OPTION>
+                            <OPTION value="Analyst">Analyst</OPTION>
+                            <OPTION value="Clerk">Clerk</OPTION>
+							<OPTION value="Salesman">Salesman</OPTION> 	
+                        </select></TD>
+                </tr>
+
+                <tr>
+                    <td width = "100"> </td>
+                    <td> </td>
+                </tr>
+
+                <tr>
+                    <td width = "100"> </td>
+                    <td>
+                        <input name = "add" type = "submit" id = "add" value = "AddEmployee">
+						<input name = "clear" type = "reset" id = "add" value = "clear">
+                    </td>
+                </tr>
+
+            </table>
+        </form>
+		</br>
+        <a href="view.php"><input type="button" value="Click here to View Employee Details"></a>
+    </center>
+</body>
+</html>
---
+++ view.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>View Employee</title>
+		<link rel='stylesheet' type='text/css' href='style.css' />
+    </head>
+     <body bgcolor='#ADD8E6'>
+
+
+    <center>
+        <table  class="tb_view" cellpadding="5">
+            <caption style="margin-bottom: 10px;">
+                <div style="border-bottom: 2px solid #006400;text-align: center;">
+                    <h1 style="color:#FF6347;font-variant: all-petite-caps;text-shadow: 0px 0px 1px #333;">Employee Details</h1>
+                </div>
+            </caption>
+            <thead>
+                <tr class="table-header"> 
+
+                    <th>EmpName</th>
+                    <th>Email</th>
+                    <th>City</th>
+                    <th>ContactNumber</th>
+                    <th>Job</th>
+                </tr>
+            </thead>
+			
+            <tbody>
+                <?php
+               /*$host = 'database-1.c6jevgf51ssc.us-east-1.rds.amazonaws.com';
+                $user = 'admin';
+                $password = '123admin';
+                $database = 'StudentDB';
+                $port = 3306;*/
+
+                $host = 'localhost';
+                $user = 'root';
+				$password = '';
+               $database = 'emp_details';
+               $port = 3306;
+                $con = mysqli_connect($host, $user, $password, $database, $port);
+                $query = "SELECT * FROM `emp`";
+                $result = mysqli_query($con, $query);
+
+                $c = 0;
+                while ($data = mysqli_fetch_array($result)) {
+                    ?>
+                    <tr>
+
+                        <td><?php echo $data['name']; ?></td>
+                        <td><?php echo $data['email']; ?></td>
+                        <td><?php echo $data['salary']; ?></td>
+                        <td><?php echo $data['phone']; ?></td>
+                        <td><?php echo $data['job']; ?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+                <?php ?>
+            </tbody>
+        </table>
+
+    </center>
+	
+
+</body>
+</html>
+
